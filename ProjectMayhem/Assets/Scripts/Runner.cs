57491c1e3e2ca9dc804df3267abb4b0ab6ffa4db
Jithin Peter <jithinpeter@PTIMAC58.local>
2016-04-23 01:15:01 +0530

    Runner prefab

---
+++ ProjectMayhem/Assets/Scripts/Runner.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum RunnerState
+{
+    Running,
+    Jumping,
+    Dieing,
+    Dead,
+    Idel
+}
+
+public class Runner : MonoBehaviour {
+
+    Animator            _animator;
+    RunnerState         _state;
+
+    public float        runningSpeed = 1f;
+    public float        jumpDistance = 1f;
+    private Vector3     _direction;
+	
+	void Start () 
+    {
+        _state = RunnerState.Idel;
+        _animator = GetComponent<Animator>();
+        _direction = new Vector3(1f,0,0);
+        Run();
+	}
+	
+    void Update()
+    {
+        if(_state == RunnerState.Running || _state == RunnerState.Jumping)
+            transform.Translate(_direction * runningSpeed);
+    }
+
+    public void Jump()
+    {
+        if(_state == RunnerState.Running)
+         _animator.SetTrigger("Jump");
+    }
+
+    public void Run()
+    {
+        if(_state != RunnerState.Running)
+            _animator.SetTrigger("Run");
+    }
+
+    public void Hit()
+    { 
+        _animator.SetTrigger("Die");
+    }
+        
+
+
+    //Animation Trigger callbacks
+    public void OnJump_Started()
+    {
+        Debug.Log("Jumping");
+        _state = RunnerState.Jumping;
+        _direction = new Vector3(1f, jumpDistance, 0f);
+    }
+    public void OnJump_Halfway()
+    {
+        _direction = new Vector3(1f, -jumpDistance, 0f);
+    }
+    public void OnRun_Started()
+    {
+        if(_state != RunnerState.Running)
+        {
+            Debug.Log("Running");
+            _state = RunnerState.Running;
+            _direction = new Vector3(1f,0f,0f);
+            transform.localPosition = new Vector3(transform.localPosition.x, 0f, 0f);
+        }
+    }
+    public void OnDieing_Started()
+    {
+        if(_state == RunnerState.Running)
+        {
+            Debug.Log("Dieing");
+            _state = RunnerState.Dieing;
+        }
+    }
+  
+    public void OnDead_Started()
+    {
+        if(_state == RunnerState.Dieing)
+        {
+            Debug.Log("Dead");
+            _state = RunnerState.Dead;
+        }
+    }
+
+}
