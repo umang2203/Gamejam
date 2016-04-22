6f6928fe1743e49b6b31345be207314de0cddfa4
Umang <umang@xcubelabs.com>
2016-04-23 01:52:57 +0530

    Parallax with tilling

---
+++ ProjectMayhem/Assets/Scripts/ParallaxManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParallaxManager : MonoBehaviour {
+
+
+    public GameObject parallaxObject;
+    public float parallaxSpeed;
+
+    float prevCameraPos;
+    float currentPos;
+    float delta;
+    float offset;
+    Vector3 offsetPosition;
+
+
+
+    public Transform cameraGo;
+
+
+    void Awake()
+    {
+        if (cameraGo == null)
+        {
+            cameraGo = Camera.main.transform;
+        }
+        parallaxObject = transform.gameObject;
+
+    }
+
+
+	// Use this for initialization
+	void Start () {
+
+        prevCameraPos = cameraGo.transform.position.x;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        currentPos = cameraGo.transform.position.x;
+
+        if(currentPos != prevCameraPos)
+        {
+            
+                delta = (currentPos - prevCameraPos) * parallaxSpeed;
+                offset = parallaxObject.transform.position.x + delta ;
+                offsetPosition = new Vector3(offset,parallaxObject.transform.position.y,parallaxObject.transform.position.z );
+                parallaxObject.transform.position = offsetPosition;
+
+            prevCameraPos = cameraGo.transform.position.x;
+
+        }
+	
+	}
+}
---
+++ ProjectMayhem/Assets/Scripts/Tilling.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tilling : MonoBehaviour {
+
+
+    public float spriteWidth;
+    Camera mainCamera;
+
+    public bool leftBG = false;
+    public bool rightBG = false;
+
+    void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+	// Use this for initialization
+	void Start () 
+    {
+        spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if( rightBG == false ||leftBG == false)
+        {
+            float cameraViewX = mainCamera.orthographicSize * Screen.width/Screen.height;
+
+            float xRight = (transform.position.x + spriteWidth/2) - cameraViewX;
+            float XLeft = (transform.position.x + spriteWidth/2) + cameraViewX;
+
+            if(mainCamera.transform.position.x >= xRight - 2 && leftBG == false)
+            {
+                CreateNewBG(1);
+                leftBG = true;
+            }
+
+            if(mainCamera.transform.position.x <= XLeft - 2 && rightBG == false)
+            {
+                CreateNewBG(-1);
+                rightBG = true;
+            }
+        }
+	}
+
+    void CreateNewBG(int Side)
+    {
+        Vector3 newPosition = new Vector3 (this.transform.position.x + spriteWidth * Side, this.transform.position.y,this.transform.position.z);
+
+        Transform newBG = Instantiate (this.transform, newPosition, transform.rotation) as Transform;
+        newBG.name = transform.name;
+
+
+        newBG.parent = transform.parent;
+        if (Side > 0) 
+        {
+            newBG.GetComponent<Tilling>().rightBG = true;
+        }
+        else 
+        {
+            newBG.GetComponent<Tilling>().leftBG = true;
+        }
+
+        if(Side > 0)
+            Destroy(this.transform.gameObject,3);
+        
+    }
+}
