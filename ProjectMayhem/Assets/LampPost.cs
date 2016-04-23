25e95cab9c7147d5d3eef054ddf13bdb944c8142
Umang <umang@xcubelabs.com>
2016-04-23 16:05:16 +0530

    Parallax change

---
+++ ProjectMayhem/Assets/LampPost.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LampPost : MonoBehaviour {
+
+    public bool IsActive;
+    public Sprite LightSprite;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
